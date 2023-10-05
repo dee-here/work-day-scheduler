970a886e1a191919b1e4a0d23397d2be27cfb948
Dee <deepakdilse@gmail.com>
2023-10-05 01:32:30 -0700

    save and get tasks from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,17 @@
 // in the html.
 
 //Handle to DOM elements
+var timeBlockContainerEl = $('#time-block-container');
+var currentDayEl = $('#currentDay');
+var saveMessageEl = $('#changesSaved');
+
+var currentDay;
+// var currentHour;
+var currentHour = dayjs().hour();
+// var currentHour = 12;
+
+//localStorage
+let taskArray = new Array(9);
 
 
 $(function () {
@@ -13,16 +24,41 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  var timeBlockContainerEl = $('#time-block-container');
-  var currentDayEl = $('#currentDay');
-  var saveMessageEl = $('#changesSaved');
 
-  var currentDay;
-  // var currentHour;
-  var currentHour = dayjs().hour();
-  // var currentHour = 12;
-  
+function saveToLocalStorage() {
+  console.log("about to save: ", taskArray, JSON.stringify(taskArray));
+var taskArrayToSave = JSON.stringify(taskArray);
+localStorage.setItem('tasks', taskArrayToSave);
 
+// get tasks from localSTorage
+loadTasksFromLocalStorage();
+}
+
+
+function loadTasksFromLocalStorage() {
+  var storedTaskArray = localStorage.getItem('tasks');
+  if(storedTaskArray) {
+    taskArray = JSON.parse(storedTaskArray);
+  }
+  console.log('storedTaskArray ', storedTaskArray);
+}
+
+function updateTasksForTimeBlocks() {
+  loadTasksFromLocalStorage();
+  var allTimeBlockDivs = timeBlockContainerEl.children(".time-block");
+  // console.log("allTimeBlock instance of Jquery: ", allTimeBlock instanceof jQuery);
+
+  for (let i = 0; i < allTimeBlockDivs.length; i++) {
+    console.log("update task with :", $(allTimeBlockDivs[i]).find('textarea').val(taskArray[i]));
+
+    $(allTimeBlockDivs[i]).find('textarea').val(taskArray[i]);
+
+  //  textareaToupdate = $(allTimeBlockDivs[i]);
+
+    // var blockId = getDivId($(allTimeBlock[i]));
+    // var blockHour = parseInt(getHourFromDivId(blockId));
+  }
+}
   function handleTimeBlockClicks(event){
     //this event has a current target that gives us the clicked div id => div#hour-9, div#hour-10, div#hour-11
     console.log("code-handleTextAreaClicks with event : ", event);
@@ -42,11 +78,34 @@ $(function () {
       var buttonClicked = $(event.target);
       console.log("buttonClicked !!", buttonClicked);
       console.log("BntClicked this!!", this);
+
+      //call method to update taskArray based on what user enetered
+      updateTaskArray(this);
+      saveToLocalStorage();
     }
 
   }
 
-timeBlockContainerEl.on('click', '.time-block', handleTimeBlockClicks);
+function updateTaskArray(div) {
+  var clickedBlock = getDivId($(div));
+  console.log("clickedBlock : ", clickedBlock);
+  var clickedHour = parseInt(getHourFromDivId(clickedBlock));
+
+  console.log("clickedHour to update : ", clickedHour);
+
+  //get textarea for task text
+  if(clickedBlock) {
+    var textareaEl = $(div).find('textarea');
+    var taskToSave = (textareaEl.val()).trim();
+    if(taskToSave != null && taskToSave.trim() != '' ) {
+      console.log("saving: ", taskToSave , " at: ", clickedHour % 9);
+      taskArray[clickedHour % 9 ] = taskToSave;
+    }
+    // console.log("textareaEl : ", textareaEl, textareaEl.val());
+  }
+}
+
+// timeBlockContainerEl.on('click', '.time-block', handleTimeBlockClicks);
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -56,26 +115,27 @@ timeBlockContainerEl.on('click', '.time-block', handleTimeBlockClicks);
 
 //Add colors to each time black
 function addColorForTimeBlocks() {
-  console.log("lets add color to: !", timeBlockContainerEl);
+  // console.log("lets add color to: !", timeBlockContainerEl);
 
-  console.log("lets add color to: !", timeBlockContainerEl.children('.time-block'));
+  // console.log("lets add color to: !", timeBlockContainerEl.children('.time-block'));
 
-  getHourFromDivId('hour-20');
+  // getHourFromDivId('hour-20');
   //var currentTime = dayjs();
   // console.log(`The current HOUR is: `, currentDay.hour());
 
   var allTimeBlock = timeBlockContainerEl.children('.time-block');
-  console.log("allTimeBlock instance of Jquery: ", allTimeBlock instanceof jQuery);
+  // console.log("allTimeBlock instance of Jquery: ", allTimeBlock instanceof jQuery);
 
   for(let i=0; i<allTimeBlock.length; i++) {
     // console.log("EACH TimeBlock instance of Jquery: ", $(allTimeBlock[i]) instanceof jQuery);
     // console.log(allTimeBlock[i]);
     // console.log($(allTimeBlock[i]));
+    allTimeBlock[i] = $(allTimeBlock[i]);
     var blockId = getDivId($(allTimeBlock[i]));
     var blockHour = parseInt(getHourFromDivId(blockId));
-    console.log("blockId : ", blockId, blockHour, currentHour);
-    console.log("checking is Jquery for alltimeblock[i] ", allTimeBlock[i] instanceof jQuery);
-    allTimeBlock[i] = $(allTimeBlock[i]);
+    // console.log("blockId : ", blockId, blockHour, currentHour);
+    // console.log("checking is Jquery for alltimeblock[i] ", allTimeBlock[i] instanceof jQuery);
+    // allTimeBlock[i] = $(allTimeBlock[i]);
     if(blockHour === currentHour) {
       console.log(" Add Current state !");
       allTimeBlock[i].removeClass('past');
@@ -87,7 +147,7 @@ function addColorForTimeBlocks() {
       allTimeBlock[i].removeClass('present');
       allTimeBlock[i].removeClass('future');
       allTimeBlock[i].addClass('past');
-    } else {
+    } else if (blockHour > currentHour) {
       console.log( "add Future !!!!"); 
       allTimeBlock[i].removeClass('present');
       allTimeBlock[i].removeClass('past');
@@ -99,16 +159,16 @@ function addColorForTimeBlocks() {
 function getHourFromDivId(id) {
 
 
-    console.log((id.split('hour-'))[1]);
+    // console.log((id.split('hour-'))[1]);
     if(id.length > 0) {
-      console.log('ID : ', id)
+      // console.log('ID : ', id)
       return (id.split('hour-'))[1];
     }
 
 }
 
 function getDivId(div) {
-  console.log('DIV : ', div.prop('id'));
+  // console.log('DIV : ', div.prop('id'));
   if(div instanceof jQuery) {
     return div.prop('id');
   }
@@ -126,7 +186,6 @@ addColorForTimeBlocks()
   //
   // TODO: Add code to display the current date in the header of the page.
 function displayTime() {
-
   //update time
   currentDay = dayjs();
   currentHour = currentDay.hour();
@@ -138,8 +197,17 @@ function displayTime() {
   addColorForTimeBlocks();
 }
 
-//add timer event to update time
-displayTime()
-setInterval(displayTime, 1000);
 
+
+//mainfunction
+function init() {
+//add timer event to update time
+timeBlockContainerEl.on('click', '.time-block', handleTimeBlockClicks);
+displayTime()
+addColorForTimeBlocks();
+updateTasksForTimeBlocks()
+setInterval(displayTime, 1000);
+}
+
+init();
 });
