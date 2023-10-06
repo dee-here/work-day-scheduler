72b3cc91500204b0f9b5e36002f4da0c01c3fae6
Dee <deepakdilse@gmail.com>
2023-10-05 20:11:37 -0700

    format code, update README

--- README.md
+++ README.md
@@ -1,99 +1,58 @@
-# 05 Third-Party APIs: Work Day Scheduler
+# Work Day Scheduler
+This project was to create a simple day planner application that allows a user to save events for a day [9am to 5pm].
 
-## Your Task
+## Technology Used 
 
-Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+| Technology Used         | Resource URL           | 
+| ------------- |-------------| 
+| jQuery   | [https://jquery.com/](https://jquery.com/) | 
+| HTML    | [https://developer.mozilla.org/en-US/docs/Web/HTML](https://developer.mozilla.org/en-US/docs/Web/HTML) | 
+| CSS     | [https://developer.mozilla.org/en-US/docs/Web/CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)      |   
+| Git | [https://git-scm.com/](https://git-scm.com/)     |    
+| Day.js | [https://day.js.org/](https://day.js.org/)     |   
 
-You'll need to use the [Day.js](https://day.js.org/en/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Day.js in the browser.
 
-## User Story
 
-```md
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
+## Description
 
-## Acceptance Criteria
+A simple day planner application that allows a user to save events for the working hours of the day. Starter code was provided and our task was to update it to get the desired functionality.
 
-```md
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-```
+User is able to see color coded time blocks that represent an hour of time and if it is the current, past or future time.
 
-The following animation demonstrates the application functionality:
+User is able to click on the text area and update the tasks for that hour of time and save it by using the save button. The changes are saved to the browsers localStorage and can persist users tasks.
 
-<!-- @TODO: create ticket to review/update image) -->
-![A user clicks on slots on the color-coded calendar and edits the events.](./Assets/05-third-party-apis-homework-demo.gif)
+When the application first loads it gets the tasks stored in the browsers localStorage and uses it to add content to the time blocks.
 
-## Grading Requirements
+The following animation demonstrates the mock application functionality:
+![color-coded calendar](./assets/images/05-third-party-apis-homework-demo.gif)
 
-> **Note**: If a Challenge assignment submission is marked as “0”, it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
+## Usage
+When a user opens this application, they are able to see the current date and time and color coded time blocks for each of the hours from 9am to 5pm. Each hour of time is color coded to show if its the present, past or future time.
 
-This Challenge is graded based on the following criteria:
+User can click on the text area and update the test for that hour. Clicking on the save button will save this to local storage and will persist even if the user refreshes the browser.
 
-### Technical Acceptance Criteria: 40%
+The application displays a message when anything is saved to localStorage. The message fades away after 5 seconds of being displayed.
 
-* Satisfies all of the above acceptance criteria plus the following:
 
-  * Uses a date utility library to work with date and time
 
-### Deployment: 32%
+## Learning points
+Here are a few things that I learnt while working on this project.
 
-* Application deployed at live URL
+Using external library like day.js to use time calculations and displaying time in different formats.
+Using jQuery to access DOM nodes and manipulating their properties and setting time intervals and timers.
 
-* Application loads with no errors
+Using CSS for hiding and displaying text using opacity and transitions.
+Saving and retreiving data from localStorage.
 
-* Application GitHub URL submitted
 
-* GitHub repo contains application code
+## Screenshots
 
-### Application Quality: 15%
+<img src="./assets/images/Day-planner-1.png" height="200" >
+<img src="./assets/images/Day-planner-2.png" height="200" >
 
-* Application user experience is intuitive and easy to navigate
 
-* Application user interface style is clean and polished
+## Author Info
+Deepak Sinha
 
-* Application resembles the mock-up functionality provided in the Challenge instructions
-
-### Repository Quality: 13%
-
-* Repository has a unique name
-
-* Repository follows best practices for file structure and naming conventions
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages
-
-* Repository contains quality README file with description, screenshot, and link to deployed application
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application
-
-* The URL of the GitHub repository, with a unique name and a README describing the project
-
-- - -
-© 2022 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
+* [Portfolio](https://dee-here.github.io/portfolio/)
+* [Github](https://github.com/dee-here)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,186 +1,128 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 //Handle to DOM elements
-var timeBlockContainerEl = $('#time-block-container');
-var currentDayEl = $('#currentDay');
-var saveMessageEl = $('#changesSaved');
+var timeBlockContainerEl = $("#time-block-container");
+var currentDayEl = $("#currentDay");
+var saveMessageEl = $("#changesSaved");
 
+// Global Variables
 var currentDay;
 var currentHour = dayjs().hour();
-
-//localStorage
 var taskArray = new Array(9);
 
 $(function () {
+  function saveToLocalStorage() {
+    var taskArrayToSave = JSON.stringify(taskArray);
+    localStorage.setItem("tasks", taskArrayToSave);
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-function saveToLocalStorage() {
-  console.log("about to save: ", taskArray, JSON.stringify(taskArray));
-var taskArrayToSave = JSON.stringify(taskArray);
-localStorage.setItem('tasks', taskArrayToSave);
-
-//display and fade save to local storage text
-fadeSavetoLocalStorageText();
-// get tasks from localSTorage
-loadTasksFromLocalStorage();
-}
-
-
-function loadTasksFromLocalStorage() {
-  var storedTaskArray = localStorage.getItem('tasks');
-  if(storedTaskArray) {
-    taskArray = JSON.parse(storedTaskArray);
+    //display and fade save to local storage text
+    fadeSavetoLocalStorageText();
+    // get tasks from localSTorage
+    loadTasksFromLocalStorage();
   }
-  console.log('storedTaskArray ', storedTaskArray);
-}
 
-function updateTasksForTimeBlocks() {
-  loadTasksFromLocalStorage();
-  var allTimeBlockDivs = timeBlockContainerEl.children(".time-block");
-  // console.log("allTimeBlock instance of Jquery: ", allTimeBlock instanceof jQuery);
-
-  for (let i = 0; i < allTimeBlockDivs.length; i++) {
-    console.log("update task with :", $(allTimeBlockDivs[i]).find('textarea').val(taskArray[i]));
-
-    $(allTimeBlockDivs[i]).find('textarea').val(taskArray[i]);
-
-  //  textareaToupdate = $(allTimeBlockDivs[i]);
-
-    // var blockId = getDivId($(allTimeBlock[i]));
-    // var blockHour = parseInt(getHourFromDivId(blockId));
+  // get tasks saved in local storage
+  function loadTasksFromLocalStorage() {
+    var storedTaskArray = localStorage.getItem("tasks");
+    if (storedTaskArray) {
+      taskArray = JSON.parse(storedTaskArray);
+    }
   }
-}
-  function handleTimeBlockClicks(event){
-    if($(event.target).is('button') || $(event.target).is('i')) {
+
+  //Get tasks from local storage and update the individual text areas.
+  function updateTasksForTimeBlocks() {
+    loadTasksFromLocalStorage();
+    var allTimeBlockDivs = timeBlockContainerEl.children(".time-block");
+    for (let i = 0; i < allTimeBlockDivs.length; i++) {
+      $(allTimeBlockDivs[i]).find("textarea").val(taskArray[i]);
+    }
+  }
+
+  //if button area is clicked or the save icon inside it.
+  function handleTimeBlockClicks(event) {
+    if ($(event.target).is("button") || $(event.target).is("i")) {
       updateTaskArray(this);
       saveToLocalStorage();
     }
   }
 
-function updateTaskArray(div) {
-  var clickedBlock = getDivId($(div));
-  console.log("clickedBlock : ", clickedBlock);
-  var clickedHour = parseInt(getHourFromDivId(clickedBlock));
+  function updateTaskArray(div) {
+    var clickedBlock = getDivId($(div));
+    var clickedHour = parseInt(getHourFromDivId(clickedBlock));
 
-  console.log("clickedHour to update : ", clickedHour);
-
-  //get textarea for task text
-  if(clickedBlock) {
-    var textareaEl = $(div).find('textarea');
-    var taskToSave = (textareaEl.val()).trim();
-    if(taskToSave != null && taskToSave.trim() != '' ) {
-      console.log("saving: ", taskToSave , " at: ", clickedHour % 9);
-      taskArray[clickedHour % 9 ] = taskToSave;
-    }
-    // console.log("textareaEl : ", textareaEl, textareaEl.val());
-  }
-}
-
-// timeBlockContainerEl.on('click', '.time-block', handleTimeBlockClicks);
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-//Add colors to each time black
-function addColorForTimeBlocks() {
-
-  var allTimeBlock = timeBlockContainerEl.children('.time-block');
-  // console.log("allTimeBlock instance of Jquery: ", allTimeBlock instanceof jQuery);
-
-  for(let i=0; i<allTimeBlock.length; i++) {
-    allTimeBlock[i] = $(allTimeBlock[i]);
-    var blockId = getDivId($(allTimeBlock[i]));
-    var blockHour = parseInt(getHourFromDivId(blockId));
-    if(blockHour === currentHour) {
-      // console.log(" Add Current state !");
-      allTimeBlock[i].removeClass('past');
-      allTimeBlock[i].removeClass('future');
-      allTimeBlock[i].addClass('present');
-
-    } else if (blockHour < currentHour) {
-      // console.log( "add PAST ###");
-      allTimeBlock[i].removeClass('present');
-      allTimeBlock[i].removeClass('future');
-      allTimeBlock[i].addClass('past');
-    } else if (blockHour > currentHour) {
-      // console.log( "add Future !!!!"); 
-      allTimeBlock[i].removeClass('present');
-      allTimeBlock[i].removeClass('past');
-      allTimeBlock[i].addClass('future');
+    //get textarea for task text
+    if (clickedBlock) {
+      var textareaEl = $(div).find("textarea");
+      var taskToSave = textareaEl.val().trim();
+      if (taskToSave != null && taskToSave.trim() != "") {
+        taskArray[clickedHour % 9] = taskToSave;
+      }
     }
   }
 
-}
-function getHourFromDivId(id) {
-
-
-    // console.log((id.split('hour-'))[1]);
-    if(id.length > 0) {
-      // console.log('ID : ', id)
-      return (id.split('hour-'))[1];
+  //Add colors to each time block based on current hour and blocks id
+  function addColorForTimeBlocks() {
+    var allTimeBlock = timeBlockContainerEl.children(".time-block");
+    for (let i = 0; i < allTimeBlock.length; i++) {
+      allTimeBlock[i] = $(allTimeBlock[i]);
+      var blockId = getDivId($(allTimeBlock[i]));
+      var blockHour = parseInt(getHourFromDivId(blockId));
+      if (blockHour === currentHour) {
+        allTimeBlock[i].removeClass("past");
+        allTimeBlock[i].removeClass("future");
+        allTimeBlock[i].addClass("present");
+      } else if (blockHour < currentHour) {
+        allTimeBlock[i].removeClass("present");
+        allTimeBlock[i].removeClass("future");
+        allTimeBlock[i].addClass("past");
+      } else if (blockHour > currentHour) {
+        allTimeBlock[i].removeClass("present");
+        allTimeBlock[i].removeClass("past");
+        allTimeBlock[i].addClass("future");
+      }
     }
-
-}
-
-function getDivId(div) {
-  // console.log('DIV : ', div.prop('id'));
-  if(div instanceof jQuery) {
-    return div.prop('id');
   }
 
-}
+  // get time from the id of the div clicked.  hour-8, hour-9, .. returns 8, 9.
+  function getHourFromDivId(id) {
+    if (id.length > 0) {
+      return id.split("hour-")[1];
+    }
+  }
 
+  function getDivId(div) {
+    return $(div).prop("id");
+  }
 
-//call method to color each block!
-// addColorForTimeBlocks()
+  function updateTime() {
+    //update time
+    currentDay = dayjs();
+    currentHour = currentDay.hour();
 
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-function updateTime() {
-  //update time
-  currentDay = dayjs();
-  currentHour = currentDay.hour();
+    //format dayjs to display date and time in header
+    currentDayEl.text(dayjs().format("dddd, MMMM D, YYYY h:mm:ss A"));
+    //add colors for each block as time updates
+    addColorForTimeBlocks();
+  }
 
-  currentDayEl.text(dayjs().format('dddd, MMMM D, YYYY h:mm:ss A'));
-  // saveMessageEl.text(currentHour);
+  function fadeSavetoLocalStorageText() {
+    //Show saved to local storage message.
+    saveMessageEl.addClass("show-message");
+    //Fade the messaage after 5 seconds.
+    setTimeout(function () {
+      saveMessageEl.removeClass("show-message");
+    }, 5000);
+  }
 
-  //add colors for each block
-  addColorForTimeBlocks();
-}
+  function init() {
+    //add event listener to each time block to handle updating tasks and finding hour
+    timeBlockContainerEl.on("click", ".time-block", handleTimeBlockClicks);
+    updateTime();
+    addColorForTimeBlocks();
+    updateTasksForTimeBlocks();
 
-function fadeSavetoLocalStorageText() {
-  //Show saved to local storage message.
-  saveMessageEl.addClass('show-message');
-  //Fade the messaage after 5 seconds.
-  setTimeout(function() {
-    saveMessageEl.removeClass('show-message');
-  }, 5000);
-}
+    // update time after every second
+    setInterval(updateTime, 1000);
+  }
 
-//mainfunction
-function init() {
-//add timer event to update time
-timeBlockContainerEl.on('click', '.time-block', handleTimeBlockClicks);
-updateTime()
-addColorForTimeBlocks();
-updateTasksForTimeBlocks()
-setInterval(updateTime, 1000);
-}
-
-init();
+  //start
+  init();
 });
